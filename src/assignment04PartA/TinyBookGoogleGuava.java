f11598abdcc9c28f16831e5854eb8d8e70ea82c6
mosguinz <mos.guinz@gmail.com>
2021-04-28 02:44:34 -0700

    Add test file for GG import

--- src/assignment04PartA/TinyBookGoogleGuava.java
+++ src/assignment04PartA/TinyBookGoogleGuava.java
@@ -1,2 +1,97 @@
-package assignment04PartA;public enum TinyBookGoogleGuava {
+package assignment04PartA;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+import java.util.Collection;
+
+/**
+ * @author Duc Ta PLUS
+ */
+public class TinyBookGoogleGuava {
+
+    public static void main(String[] args) {
+        Multimap<String, String> tinyBook = ArrayListMultimap.create();
+
+        // Adding some key/value
+        System.out.println("\nAdding----------------------------------------");
+        tinyBook.put("book", "A set of pages.");
+        tinyBook.put("book", "To arrange something on a particular date.");
+        tinyBook.put("bookable", "Can be ordered.");
+        tinyBook.put("bookbinder", "A person who fastens the pages of books.");
+        tinyBook.put("bookcase", "A piece of furniture with shelves.");
+
+        // Getting the size
+        System.out.println("\nSize------------------------------------------");
+        System.out.println(tinyBook.size());  // 5
+
+        // Getting values
+        System.out.println("\nValues----------------------------------------");
+        Collection<String> book = tinyBook.get("book");
+        System.out.println(book);
+        // [A set of pages., To arrange something on a particular date.]
+
+        Collection<String> bookcase = tinyBook.get("bookcase");
+        System.out.println(bookcase);
+        // [A piece of furniture with shelves.]
+
+        // Iterating over entire Mutlimap
+        System.out.println("\nIterate---------------------------------------");
+        tinyBook.values().forEach((value) -> {
+            System.out.println(value);
+        });
+
+        // Getting hash codes
+        System.out.println("\nHash Codes-------------------------------------");
+        tinyBook.entries().forEach((entry) -> {
+            System.out.println(entry.hashCode() + "\t"
+                    + System.identityHashCode(entry));
+        });
+
+        System.out.println("\nHash Code--------------------------------------");
+        System.out.print(tinyBook.hashCode() + "\t");
+        System.out.println(System.identityHashCode(tinyBook));
+
+        // Removing a single value
+        System.out.println("\nRemove----------------------------------------");
+        tinyBook.remove("bookcase", "A piece of furniture with shelves.");
+        System.out.println(tinyBook.get("bookcase")); // []
+
+        // Remove all values for a key
+        System.out.println("\nRemove All------------------------------------");
+        tinyBook.removeAll("book");
+        System.out.println(tinyBook.get("book")); // []
+    }
 }
+/* OUTPUT
+Adding----------------------------------------
+
+Size------------------------------------------
+5
+
+Values----------------------------------------
+[A set of pages., To arrange something on a particular date.]
+[A piece of furniture with shelves.]
+
+Iterate---------------------------------------
+Can be ordered.
+A set of pages.
+To arrange something on a particular date.
+A person who fastens the pages of books.
+A piece of furniture with shelves.
+
+Hash Codes-------------------------------------
+-920681491	625576447
+1244648863	1560911714
+-148441025	939047783
+-323823247	1237514926
+-238541373	548246552
+
+Hash Code--------------------------------------
+-1794801272	401625763
+
+Remove----------------------------------------
+[]
+
+Remove All------------------------------------
+[]
+*/
