4943dfdd215e4df84838b31bbce91aa7b406a328
mosguinz <mos.guinz@gmail.com>
2021-04-29 01:19:52 -0700

    Add Enum for DictEntry

---
+++ src/assignment04PartBAndC/DictEntry.java
@@ -0,0 +1,50 @@
+package assignment04PartBAndC;
+
+public enum DictEntry {
+
+    BOOK_N("book", "A written work published in printed or electronic form.", PartOfSpeech.NOUN),
+    BOOK_V("book", "To arrange for someone to have a seat on a plane.", PartOfSpeech.VERB),
+    BOOKABLE("bookable", "Can be ordered in advance.", PartOfSpeech.ADJECTIVE),
+    BOOKCASE("bookcase", "A piece of furniture with shelves.", PartOfSpeech.NOUN),
+    BOOKBINDER("bookbinder", "A person who fastens the pages of books.", PartOfSpeech.NOUN),
+    CSC220_A("CSC220", "Ready to create complex data structures.", PartOfSpeech.ADJECTIVE),
+    CSC220_N("CSC220", "Data Structures.", PartOfSpeech.NOUN),
+    CSC220_V("CSC220", "To create data structures.", PartOfSpeech.VERB);
+
+    private String key, value;
+    private PartOfSpeech partOfSpeech;
+
+    DictEntry(String key, String value, PartOfSpeech partOfSpeech) {
+        this.key = key;
+        this.value = value;
+        this.partOfSpeech = partOfSpeech;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public PartOfSpeech getPartOfSpeech() {
+        return partOfSpeech;
+    }
+
+    public void setPartOfSpeech(PartOfSpeech partOfSpeech) {
+        this.partOfSpeech = partOfSpeech;
+    }
+}
+
+enum PartOfSpeech {
+    VERB, NOUN, ADJECTIVE;
+}
