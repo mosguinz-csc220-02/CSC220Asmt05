a4dcabd7ed59cd47d5f0fa38ba4a95b50bd33faf
mosguinz <mos.guinz@gmail.com>
2021-04-30 22:43:16 -0700

    Add method to load entries from Enum

--- src/assignment04PartBAndC/Dictionary.java
+++ src/assignment04PartBAndC/Dictionary.java
@@ -1,5 +1,16 @@
 package assignment04PartBAndC;
 
-public class Dictonary {
+import assignment04PartBAndC.DictEntry.POS;
+import com.google.common.collect.ArrayListMultimap;
+
+public class Dictionary {
+
+    ArrayListMultimap<String, ArrayListMultimap<POS, String>> entries;
+
+    private void loadEntries() {
+        for (Words word : Words.values()) {
+            entries.put(word.getKey(), word.getDefinitions());
+        }
+    }
 
 }
