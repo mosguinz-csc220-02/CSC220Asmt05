db3be9e4507748e796162ed86b17571fd7af036f
mosguinz <mos.guinz@gmail.com>
2021-05-07 02:53:15 -0700

    Add DictClient class
     - This class will be responsible for handling user interaction,
       as well as managing the different scenarios according to the
       app version.

--- src/assignment04PartBAndC/DictClient.java
+++ src/assignment04PartBAndC/DictClient.java
@@ -1,2 +1,27 @@
-package assignment04PartBAndC;public class DictClient {
+package assignment04PartBAndC;
+
+import java.util.Scanner;
+
+public class DictClient {
+
+    private static final Scanner scan = new Scanner(System.in);
+    private final VERSION APP_VERSION;
+
+    public DictClient(VERSION version) {
+        APP_VERSION = version;
+        System.out.printf("%n- DICTIONARY 220 JAVA %s -----%n"
+                + "-----     powered by Google Guava -%n", APP_VERSION);
+    }
+
+
+    private String promptInput() {
+        System.out.print("Search: ");
+        return "";
+    }
+
+
+    enum VERSION {
+        STANDARD, PROFESSIONAL
+    }
+
 }
