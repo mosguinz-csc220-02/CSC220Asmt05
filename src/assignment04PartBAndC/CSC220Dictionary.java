8f701028484e71ffd8207c2231b1a03a32d1eae7
mosguinz <mos.guinz@gmail.com>
2021-05-06 21:19:07 -0700

    Rename Dictionary -> CSC220Dictionary
     - Renamed to prevent confusion with builtin modules.

--- src/assignment04PartBAndC/CSC220Dictionary.java
+++ src/assignment04PartBAndC/CSC220Dictionary.java
@@ -3,11 +3,11 @@ package assignment04PartBAndC;
 import com.google.common.collect.ImmutableListMultimap;
 import java.util.HashMap;
 
-public class Dictionary {
+public class CSC220Dictionary {
 
     HashMap<String, ImmutableListMultimap<POS, String>> entries = new HashMap<>();
 
-    public Dictionary() {
+    public CSC220Dictionary() {
         for (Words word : Words.values()) {
             entries.put(word.getKey(), word.getDefinitions());
         }
